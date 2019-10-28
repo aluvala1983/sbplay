e40854877f84c6a6a18d8debf3468d6d7eef3ffc
aluvala1983 <mahesh.aluvala@gmail.com>
2019-10-28 19:52:21 +0530

    Fresh files

---
+++ src/main/java/my/maheshguru/sbplay/sbplay/SbplayApplication.java
@@ -0,0 +1,13 @@
+package my.maheshguru.sbplay.sbplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/maheshguru/sbplay/sbplay/SbplayApplicationTests.java
@@ -0,0 +1,16 @@
+package my.maheshguru.sbplay.sbplay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbplayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
